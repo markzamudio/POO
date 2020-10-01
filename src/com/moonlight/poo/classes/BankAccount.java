207a63c739481856a0b7f80435e2360f99c960f9
Mark Zamudio <markzamudio@hotmail.com>
2020-10-01 08:36:56 -0500

    Se agrega la clase cuenta bancaria

---
+++ src/com/moonlight/poo/classes/BankAccount.java
@@ -0,0 +1,50 @@
+package com.moonlight.poo.classes;
+
+/**
+ *
+ * @author Mark_Zamudio
+ */
+
+public class BankAccount {
+    private String accountNumber;
+    private String titular;
+    private double balance;
+    
+    public BankAccount() {
+        accountNumber = "Sin definir";
+        titular = "Sin definir";
+    }
+    
+    public BankAccount(String accountNumber, String titular, double balance) {
+        this.accountNumber = accountNumber;
+        this.titular = titular;
+        this.balance = balance;
+    }
+    
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+    
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+    
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+    
+    public String getTitular() {
+        return titular;
+    }
+    
+    public double getBalance() {
+        return balance;
+    }
+    
+    @Override
+    public String toString() {
+        return "No.Cuenta: " + this.accountNumber + "\n" +
+                "Titular: " + this.titular + "\n" +
+                "Saldo actual: " + this.balance;
+    }
+}
