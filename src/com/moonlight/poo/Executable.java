cf652a0354dfceeb0be6bc5f2ddb8874596b4f25
Mark Zamudio <markzamudio@hotmail.com>
2020-10-01 08:56:22 -0500

    Se agregan dos instancias en la clase ejecutable y se prueban su metodos y atributos

--- src/com/moonlight/poo/Executable.java
+++ src/com/moonlight/poo/Executable.java
@@ -1,5 +1,7 @@
 package com.moonlight.poo;
 
+import com.moonlight.poo.classes.*;
+
 /**
  *
  * @author Mark_Zamudio
@@ -7,6 +9,26 @@ package com.moonlight.poo;
 
 public class Executable {
     public static void main(String[] args) {
-        
+//        BankAccount persona01 = new BankAccount("398416","Pablo Garcia Castillo",500);
+//        BankAccount persona02 = new BankAccount("921465","Vanessa Ortiz guzman",200);
+//        
+//        System.out.println("**********Cuentas bancarias**********");
+//        System.out.println(persona01.toString());
+//        System.out.println("*************************************");
+//        System.out.println(persona02.toString());
+//        System.out.println("*************************************");
+//        System.out.println("Deposita persona 1 50 y retira 120");
+//        persona01.deposit(50);
+//        persona01.withdrawals(120);
+//        System.out.println("Persona 1 su estado actual");
+//        System.out.println(persona01.toString());
+//        System.out.println("*************************************");
+//        System.out.println("Deposita persona 2 75 y retira 20");
+//        persona02.deposit(75);
+//        persona02.withdrawals(20);
+//        System.out.println("Persona 2 su estado actual");
+//        System.out.println(persona02.toString());
+//        
+//        System.out.println("*************************************");
     }
 }
