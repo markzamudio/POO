09bdaa84791d03dea95e936d8b443d83021e3190
Mark Zamudio <markzamudio@hotmail.com>
2020-10-01 14:47:29 -0500

    Se agregan los metodos agregar, mostrar y buscar a la clase biblioteca

--- src/com/moonlight/poo/classes/Library.java
+++ src/com/moonlight/poo/classes/Library.java
@@ -22,4 +22,30 @@ private Publication[] shelves;
     public Publication[] getShelves() {
         return shelves;
     }
+    
+    public boolean add(Publication publication) {
+        if (index < shelves.length) {
+            this.shelves[index++] = publication;
+            return true;
+        }
+        
+        return false;    
+    }
+    
+    public void showShelves(){
+        System.out.println("Publicaciones: ");
+        for (int i = 0; i < index; i++) {
+            System.out.println(shelves[i].getTitle());
+        }
+    }
+    
+    public String searchPublication(String title) {
+        for (int i = 0; i < index; i++) {
+            if (shelves[i].getTitle().equalsIgnoreCase(title)) {
+                return "La revista se encuentra en la pos " + (i+1);
+            }
+        }
+        
+        return "No se encontro la publicacion";
+    }
 }
