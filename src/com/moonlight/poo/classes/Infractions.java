956e3149f391b91ab34c64c39adf866678b21481
Mark Zamudio <markzamudio@hotmail.com>
2020-10-04 14:57:13 -0500

    Se agrega los metodos calcular monto, conteo de multas y obtener multar a la clase multas

--- src/com/moonlight/poo/classes/Infractions.java
+++ src/com/moonlight/poo/classes/Infractions.java
@@ -21,4 +21,34 @@ public class Infractions {
         
         return false;
     }
+    
+    public double calculateAmount() {
+        double total = 0;
+        
+        for (int i = 0; i < index; i++) {
+            total += infractions[i].getAmount();
+        }
+        
+        return total;
+    }
+    
+    public int[] getInfractionCount() {
+        int counters[] = new int[20];
+        
+        for (int i = 0; i < index; i++) {
+            counters[infractions[i].getCode() - 1]++;
+        }
+        
+        return counters;
+    }
+    
+    public String getInfractions() {
+        StringBuilder temp = new StringBuilder("");
+        
+        for (int i = 0; i < index; i++) {
+            temp.append(infractions[i]).append("\n");
+        }
+        
+        return temp.toString();
+    }
 }
